546844367ab2d68d8514f9c5c0f2d99385873a71
Ali Razfar <arazfar@linkedin.com>
2016-04-23 18:26:57 -0700

    updated collect data javascript

--- app/js/collect_data.js
+++ app/js/collect_data.js
@@ -1,5 +1,20 @@
 $(function() {
   $('#the_form').submit(function(e){
     e.preventDefault();
+
+    var job_title = $('#job_title').val();
+    var city = $('#city').val();
+    var state = $('#state').val();
+    var salary = $('#salary').val();
+
+    if(job_title && city && state) {
+      $.get('http://glebslink.com', {
+        job_title: job_title,
+        city: city,
+        state: state
+      }).done(function(data) {
+        console.log(data);
+      });
+    }
   });
 });
